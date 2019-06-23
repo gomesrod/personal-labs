69baeb58d936e8158fda5678c3eb10133303d9fe
Rodrigo Gomes <rodrigo.gomes@intelie.com.br>
2019-06-23 16:26:49 -0300

    weather-service-spring

---
+++ java8microservices/weather-service-spring/src/main/java/com/packtpub/microservices/weather/WeatherServiceApplication.java
@@ -0,0 +1,11 @@
+package com.packtpub.microservices.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherServiceApplication.class);
+    }
+}
